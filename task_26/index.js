c9ae12891adbc731dec92890fb724347a004ca5c
asad-bukhari <asadbukhari65813@gmail.com>
2023-02-19 19:16:26 +0500

    Task

--- task_26/index.js
+++ task_26/index.js
@@ -1,27 +1,17 @@
 // Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
-
-
 // • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
-
-
-let alien_color = "green";
-
-if(alien_color==="green"){
+var alien_color = "green";
+if (alien_color === "green") {
     console.log("You earned 5 points");
 }
-
 // • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
-
-
-if(alien_color!=="green"){
+if (alien_color !== "green") {
     console.log("you earned 10 points");
 }
-
 // • Write one version of this program that runs the if block and another that runs the else block.
-
-if(alien_color==="green"){
+if (alien_color === "green") {
     console.log("you just earned 5 points");
 }
-else{
+else {
     console.log("you earned 10 points");
 }
--- task_27/index.js
+++ task_27/index.js
@@ -1,34 +1,24 @@
 // Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
-
-
 // • If the alien is green, print a message that the player earned 5 points.
-
-
 var alien_color = "yellow";
-if(alien_color==="yellow"){
+if (alien_color === "yellow") {
     console.log("player earned 10 points .");
-
 }
-
 // • If the alien is red, print a message that the player earned 15 points.
-
 var alien_color = "red";
-if(alien_color==="red"){
+if (alien_color === "red") {
     console.log("player earned 15 points .");
-
 }
-
 // • Write three versions of this program, making sure each message is printed for the appropriate color alien.
-
-if(alien_color==="green"){
+if (alien_color === "green") {
     console.log("You Earned 5 points");
 }
-else if(alien_color==="yellow"){
+else if (alien_color === "yellow") {
     console.log("You Earned 10 points");
 }
-else if(alien_color==="red"){
+else if (alien_color === "red") {
     console.log("You Earned 15 points");
 }
-else{
+else {
     console.log('nothing');
 }
